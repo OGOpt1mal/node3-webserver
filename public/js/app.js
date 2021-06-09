c83df6c76e0afa93a2e736de059684ba385c4f5f
Shawn Thorpe <shawnthorpe@MacBook-Pro.local>
2021-06-09 16:00:26 -0400

    final

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,11 @@
-const weatherForm = document.querySelector('form');
+const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
 const messageTwo = document.querySelector('#message-two')
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
-    messageOne.textContent = "loading...";
+    messageOne.textContent = "loading..."
     messageTwo.textContent = ''
     
     const location = search.value
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (event) => {
     fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if(data.error) {
-                messageOne.textContent = data.error;
+                messageOne.textContent = data.error
             } else {
                 messageOne.textContent = `The location is ${data.location}`
                 messageTwo.textContent =  `The forecast is ${data.forecast}`
